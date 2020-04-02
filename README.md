dc564f48ecd1271cc1f97bfea9afa800dd474a2c
Henry <45953538+Henryn21@users.noreply.github.com>
2020-04-02 14:04:32 -0700

    Added functions
    showPage- hides pages
    appendPageLinks - adds buttons that call showPage

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # td-project-2
  List pagination
+Displays a list in groups of 10 with links to the next part of list at bottom
--- js/script.js
+++ js/script.js
@@ -2,49 +2,48 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+let studentsList=document.querySelectorAll(".student-item");//store student li's
+let items=10;//items per page
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//parameters: list:list of students, page:current page
+//hides all list items, except at the index range (start-end)
+function showPage(list, page){
+   let start=(page*items)-items;
+   let end=page*items;
+   console.log("Start end",start , end);
+   let total=list.length;//length of list
+   //loop through whole list
+   for(let i=0;i<total;i++){
+      if(i<end&&i>=start){//if index is at the start and under the end 
+         // console.log(list[i], i);//display it
+         list[i].style.display="";
+      }
+      else{//else hide it
+         // console.log("Hidden",list[i], i);
+         list[i].style.display="none";
+      }
+   }
+}
+//a function to call showPage using the selected button(page number)
+function appendPageLinks(){
+   let links=studentsList.length/items+1;//# of pages is groups of 10 extra //+1 for loop to run on last few under 10
+   let buttonBox=document.createElement('div');//holds buttons
+   buttonBox.className="pagination";
+   let buttonList=document.createElement('ul');
+   buttonBox.appendChild(buttonList);
+   document.querySelector(".page").appendChild(buttonBox);
+   //create buttons
+   for(let i=1;i<links;i++){//for each link, add a button with the number
+      let buttonLink=document.createElement('a');
+      let button=document.createElement('li');
+      buttonLink.innerHTML=i;//set text to page number "i"
+      //add event listner
+      buttonLink.addEventListener("click", () =>{showPage(studentsList, i)});
+      //add to DOM
+      button.appendChild(buttonLink);
+      buttonList.appendChild(button);
+   }
 
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+}
+appendPageLinks();
